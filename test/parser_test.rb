93a2700218e4373bc641bbd56635102c330a05c5
mansakondo <mansakondo22@gmail.com>
2021-01-01 19:31:20 +0100

    Add custom Node class

---
+++ test/parser_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ParserTest < Minitest::Test
+  include TestHelper
+
+  def test_should_return_a_custom_node
+    node = parse("class Node; end")
+    assert node.is_a? Koto::Parser::AST::Node
+  end
+end
